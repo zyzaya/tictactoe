7eb9a1cbf2d0a75a3ede0db761e453356002b3d2
Riley Sherlock <riley.sherlock@mts.net>
2021-08-02 12:45:30 -0500

    Format code to rubocop standards

--- input.rb
+++ input.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Input
   attr_reader :exit_code
 
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,17 +1,19 @@
-require_relative 'Input.rb'
+# frozen_string_literal: true
+
+require_relative 'input'
 
 class TicTacToe
   X = 'x'
   O = 'o'
-  YES_INPUT = ['y', 'yes']
-  NO_INPUT = ['n', 'no']
+  YES_INPUT = %w[y yes].freeze
+  NO_INPUT = %w[n no].freeze
   INVALID_INPUT = 'Invalid input. Please try again.'
   WIN_VALUES = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
-  ]
-  EXIT_INPUT = ['exit', 'e']
+  ].freeze
+  EXIT_INPUT = %w[exit e].freeze
 
   def initialize
     reset_game
@@ -22,53 +24,60 @@ class TicTacToe
   def start_game
     reset_game
     winner = false
-    quit = false
-    until winner || quit
-      puts get_board
+    until winner
+      puts board
       break unless play_turn(X)
-      puts get_board
+
+      puts board
       winner = check_for_winner
-      unless winner 
-        break unless play_turn(O)
-        winner = check_for_winner
-      end
+      break if winner
+      break unless play_turn(O)
+
+      winner = check_for_winner
     end
+    return unless winner
+
     again = @input.get_input("#{winner} wins! Play again? (y/n)", INVALID_INPUT, YES_INPUT + NO_INPUT)
     start_game if YES_INPUT.include?(again)
   end
-  
-  def get_board
-    <<~board 
-      
+
+  def board
+    <<~BOARD
+
       #{@values[6]} | #{@values[7]} | #{@values[8]}
       #{@values[3]} | #{@values[4]} | #{@values[5]}
       #{@values[0]} | #{@values[1]} | #{@values[2]}
 
-    board
+    BOARD
   end
 
   private
+
   def play_turn(player)
     valid_input = @values.select do |v|
       v.downcase != X && v.downcase != O
     end
     cell = @input.get_input("#{player}'s turn.", INVALID_INPUT, valid_input)
     return false if @input.exit_code.include?(cell)
+
     @values[cell.to_i - 1] = player
   end
 
   def check_for_winner
     winner = false
-    WIN_VALUES.each {|w|
-      winner = X if w.all? {|v| @values[v] == X}
-      winner = O if !winner && w.all? {|v| @values[v] == O}
+    WIN_VALUES.each do |w|
+      if w.all? { |v| @values[v] == X }
+        winner = X
+      elsif w.all? { |v| @values[v] == O }
+        winner = O
+      end
       break if winner
-    }
+    end
     winner
   end
-  
+
   def reset_game
-    @values = [*1..9].map {|v| v.to_s}
+    @values = [*1..9].map(&:to_s)
   end
 end
 
